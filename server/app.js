fd1be74b0fad8f01417b4f95d5624cd7beae4567
sujit kumar <sujit.kumar@navaratan.com>
2017-11-21 16:12:38 +0530

    node setup

---
+++ server/app.js
@@ -0,0 +1,11 @@
+
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+// const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+app.listen(8080, function () {
+	console.log('Example app listening on port 8080!');
+});
+
